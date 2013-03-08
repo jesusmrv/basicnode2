e9454ead6e400b1798d6c7c0e10aec8ddecbcfeb
jesusmrv <jesusmrv@gmail.com>
2013-03-08 00:48:45 -0800

    hi techdays

--- Server.js
+++ Server.js
@@ -2,5 +2,5 @@ var http = require('http')
 var port = process.env.PORT || 1113;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello  Node123123\n');
+  res.end('Hello  Node \n');
 }).listen(port);
